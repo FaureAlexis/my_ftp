eea390b18dd5da9108bc8a7be51380d1e813e0bc
Alexis <75880156+FaureAlexis@users.noreply.github.com>
2023-03-02 11:04:10 +0100

    Fix indent

--- include/ftp.h
+++ include/ftp.h
@@ -6,24 +6,24 @@
 */
 
 #ifndef FTP_H
-#define FTP_H
-#include <arpa/inet.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/select.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include "logger.h"
+    #define FTP_H
+    #include <arpa/inet.h>
+    #include <dirent.h>
+    #include <errno.h>
+    #include <fcntl.h>
+    #include <netinet/in.h>
+    #include <signal.h>
+    #include <stdbool.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <sys/select.h>
+    #include <sys/socket.h>
+    #include <sys/stat.h>
+    #include <sys/types.h>
+    #include <sys/wait.h>
+    #include <unistd.h>
+    #include "logger.h"
 
 typedef struct ftp_s {
     int server_socket;
