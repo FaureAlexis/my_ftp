095de43e90b9db79c46e8c594d1b75f8f37c8f03
Alexis Faure <alexis.faure@epitech.eu>
2023-03-08 14:18:29 +0100

    Fix coding style

--- src/commands/pasv.c
+++ src/commands/pasv.c
@@ -7,24 +7,14 @@
 
 #include "../../include/ftp.h"
 
-int pasv(int client_socket)
+int create_pasv_socket(void)
 {
-    int port = 0;
-    int pasv_socket = 0;
-    int pasv_client_socket = 0;
-    struct sockaddr_in pasv_server_address;
-    struct sockaddr_in pasv_client_address;
-    socklen_t pasv_client_address_size = sizeof(pasv_client_address);
-    char *port_str = malloc(1024);
-    char *pasv_str = malloc(1024);
-
-    memset(port_str, 0, 1024);
-    memset(pasv_str, 0, 1024);
-    pasv_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int pasv_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (pasv_socket == -1) {
         perror("socket");
-        return 84;
+        return -1;
     }
+    struct sockaddr_in pasv_server_address = {0};
     pasv_server_address.sin_family = AF_INET;
     pasv_server_address.sin_addr.s_addr = INADDR_ANY;
     pasv_server_address.sin_port = htons(0);
@@ -32,27 +22,62 @@ int pasv(int client_socket)
             sizeof(pasv_server_address))
         < 0) {
         perror("bind");
-        return 84;
+        return -1;
     }
     if (listen(pasv_socket, 1) < 0) {
         perror("listen");
-        return 84;
+        return -1;
     }
+    return pasv_socket;
+}
+
+int get_pasv_port(int pasv_socket, char *port_str)
+{
+    struct sockaddr_in pasv_server_address = {0};
+    socklen_t pasv_server_address_size = sizeof(pasv_server_address);
     if (getsockname(pasv_socket, (struct sockaddr *) &pasv_server_address,
-            &pasv_client_address_size)
+            &pasv_server_address_size)
         < 0) {
         perror("getsockname");
-        return 84;
+        return -1;
     }
-    port = ntohs(pasv_server_address.sin_port);
+    int port = ntohs(pasv_server_address.sin_port);
     sprintf(port_str, "%d", port);
-    sprintf(pasv_str, "227 Entering Passive Mode (%s,%s,%s)\r\n", "127.0.0.1",
-        port_str, port_str);
-    write_client(client_socket, pasv_str);
-    pasv_client_socket = accept(pasv_socket,
+    return port;
+}
+
+int accept_pasv_client(int pasv_socket)
+{
+    struct sockaddr_in pasv_client_address = {0};
+    socklen_t pasv_client_address_size = sizeof(pasv_client_address);
+    int pasv_client_socket = accept(pasv_socket,
         (struct sockaddr *) &pasv_client_address, &pasv_client_address_size);
     if (pasv_client_socket < 0) {
         perror("accept");
+        return -1;
+    }
+    return pasv_client_socket;
+}
+
+int pasv(int client_socket)
+{
+    char *port_str = malloc(1024);
+    char *pasv_str = malloc(1024);
+    memset(port_str, 0, 1024);
+    memset(pasv_str, 0, 1024);
+    int pasv_socket = create_pasv_socket();
+    if (pasv_socket < 0) {
+        return 84;
+    }
+    int port = get_pasv_port(pasv_socket, port_str);
+    if (port < 0) {
+        return 84;
+    }
+    sprintf(pasv_str, "227 Entering Passive Mode (%s,%s,%s)\r\n", "127.0.0.1",
+        port_str, port_str);
+    write_client(client_socket, pasv_str);
+    int pasv_client_socket = accept_pasv_client(pasv_socket);
+    if (pasv_client_socket < 0) {
         return 84;
     }
     return pasv_client_socket;
